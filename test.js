e662ad15433eb6aeee29c4bf7e81e62e8ebf0058
SyndicationXI <ucrykilla@yahoo.com>
2015-10-14 22:24:24 -0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,36 @@
+// Check if the user is ready to play!
+
+confirm("I am ready to play!");
+
+var age = prompt("What's your age?");
+
+if( age < 13 ) 
+{
+    confirm("I am not liable for any damages caused by my text-based game.");
+}
+else
+{
+    confirm("Why da fook are ye still 'ere? Go on fag, play me game.");
+}
+
+confirm( "You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'" );
+
+confirm("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+
+var userAnswer = prompt( "Do you want to race Bieber on stage?");
+
+if ( userAnswer === "yes" ) {
+    console.log( "You and Bieber start racing. It's neck and neck! You win by a shoelace!" );
+} else {
+    console.log( "Oh no! Bieber shakes his head and sings 'I set a pace, so i can race without pacing.'" );
+}
+
+var feedback = prompt ( "Pls rate me game out of 10 Whoopies." )
+if ( feedback >= 8 )
+{
+    console.log( "Awwwwww, thx m8ies." );
+}
+else
+{
+    console.log( "You are a piece of fucking fuck you fucking fuck go fucking kill yourself and rot in hell you fucking fuck of fuck." );
+}
